94916c2c9b394ae7c374d2538ea30bea4c0efa7d
15ivan15 <ivanmolina15xd1@gmail.com>
2021-08-11 13:57:42 -0500

    actualizado

--- prueba1-contador/src/App.vue
+++ prueba1-contador/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-  </div>
+    <!--img alt="Vue logo" src="./assets/logo.png"-->
+    <counter></counter>
+  </div>    
 </template>
 
 <script>
-
+import Counter from '@/components/Counter'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Counter
   }
 }
 </script>
+ 
---
+++ prueba1-contador/src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1 class="text-center">
+            Esto es una prueba, espero que sirva men
+        </h1>
+        <button class="btn btn-success btn-block" @click="increment()"> Incrementa</button>
+        <div class="alert alert-primary mt-3" role="alert">
+            {{counter}}
+        </div>
+        <button class="btn btn-danger btn-block" @click="decrement()"> Decrementa</button>
+    </div>
+</template>
+
+<script>
+import {mapState, mapMutations} from 'vuex'
+
+export default {
+  name: 'Counter',
+  computed: {
+      ...mapState(['counter'])
+  },
+  methods:{
+      ...mapMutations(['increment','decrement'])
+  }
+}
+</script>
