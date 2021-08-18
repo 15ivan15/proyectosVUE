17744eeb4a4df223d02c260d7c06c686f5dadc90
15ivan15 <ivanmolina15xd1@gmail.com>
2021-08-18 12:09:45 -0500

    18-08

--- Book-It-frontend/src/views/Usuario.vue
+++ Book-It-frontend/src/views/Usuario.vue
@@ -2,49 +2,34 @@
   <div class="usuario">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-12">
-          <div class="row">
-            <div class="col-md-12">
-                <br><br><br><br>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-4">
-              <h1 class="text-primary text-center" style="font-size: 350%">Book-It</h1>
-              <p class="lead text-muted text-center">
-                  <br><br>
-                Lorem ipsum dolor sit amet,
-                <strong>consectetur adipiscing elit</strong>. Aliquam eget
-                sapien sapien. Curabitur in metus urna. In hac habitasse platea
-                dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam
-                purus nibh, lacinia non faucibus et, pharetra in dolor. Sed
-                iaculis posuere diam ut cursus.
-                <em
-                  >Morbi commodo sodales nisi id sodales. Proin consectetur,
-                  nisi id commodo imperdiet, metus nunc consequat lectus, id
-                  bibendum diam velit et dui.</em
-                >
-                Proin massa magna, vulputate nec bibendum nec, posuere nec
-                lacus.
-                <small
-                  >Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit.
-                  Nulla euismod ultrices massa, et feugiat ipsum consequat
-                  eu.</small
-                >
-              </p>
-            </div>
-            <div class="col-md-4"></div>
-          </div>
-          <div class="row">
-            <div class="col-md-12"></div>
-          </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-4">
+          <br /><br />
+          <img
+            alt="Bootstrap Image Preview"
+            src="../assets/profile.png"
+            class="rounded-circle"
+          />
+          <dl>
+            <br />
+            <dt>Nombre completo:</dt>
+            <dd>A description list is perfect for defining terms.</dd>
+            <dt>Usuario:</dt>
+            <dd>
+              Vestibulum id ligula porta felis euismod semper eget lacinia odio
+              sem nec elit.
+            </dd>
+            <dt>Correo electrónico:</dt>
+            <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
+          </dl>
+          <br><br>
+          <b-button pill variant="danger">Eliminar cuenta</b-button>
         </div>
+        <div class="col-md-7"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 </script>
